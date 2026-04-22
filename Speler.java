186a4d00dceea92591f9d288b78fc06ebb5d6883
RPTolivierV <olivier.vandamme@leerling.molenland.be>
2026-04-22 11:00:18 +0200

    speler

--- Speler.java
+++ Speler.java
@@ -1,33 +1,13 @@
-
-/**
- * class Speler - geef hier een beschrijving van deze class
- *
- * @author (jouw naam)
- * @version (versie nummer of datum)
- */
 public class Speler
 {
-    // instance variables - vervang deze door jouw variabelen
-    private int x;
+    private String naam;
+    private Blokje blokjes;
+    private int punten;
+    private Bord bord;
 
-    /**
-     * Constructor voor objects van class Speler
-     */
-    public Speler()
+    public Speler(String naam)
     {
-        // geef de instance variables een beginwaarde
-        x = 0;
-    }
-
-    /**
-     * Voorbeeld van een method - schrijf hier jouw comment
-     *
-     * @param  y    deze method krijgt deze parameter mee in de aanroep
-     * @return    deze method geeft de som van x en y terug
-     */
-    public int voorbeeldMethod(int y)
-    {
-        // schrijf hier jouw code
-        return x + y;
+        this.naam = naam;
+        punten = 0;
     }
 }
