793363d687bc2d350966aa528278965b76c198c3
RPTArneV <arne.verhaeghe@leerling.molenland.be>
2026-04-29 10:37:27 +0200

    Bord

---
+++ Bord.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Bord<Blokjes> {
+    private ArrayList<Blokje> blokjes;
+
+    public Bord()
+    {
+        blokjes = new ArrayList<Blokje>();
+    }
+    
+    
+}
