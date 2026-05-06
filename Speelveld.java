1ef2192ba8ec152aac7c38e4c948d9d4bc84685d
yero95 <yero.beeusaert@leerling.molenland.be>
2026-05-06 10:38:19 +0200

    methode voegBlokjeToe en verwijderBlokje

--- Speelveld.java
+++ Speelveld.java
@@ -7,5 +7,35 @@
  */
 public class Speelveld
 {
-    private int[][] Speelveld = new int[15][8];
+    private Blokje[][] Speelveld = new Blokje[15][8];
+    private Blokje blokje;
+    
+    boolean positieCheck(int rij, int kolom){
+        boolean positieCorrect = false;
+        if(rij >= 0 && rij <= Speelveld.length){
+            if(kolom >= 0 && kolom <= Speelveld.length){
+                positieCorrect = true;
+            }
+        }
+        return positieCorrect;
+    }
+    
+    boolean validBlokjeCombo(Blokje b){
+        boolean valid = false;
+        if(){
+            
+        }
+    }
+    
+    public void voegBlokjeToe(Blokje b, int rij, int kolom){
+        if(positieCheck(rij, kolom)){
+            Speelveld[rij][kolom] = b;
+        }
+    }
+    
+    public void verwijderBlokje(int rij, int kolom){
+        if(positieCheck(rij, kolom)){
+            Speelveld[rij][kolom] = null;
+        }
+    }
 }
