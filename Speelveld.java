efbbc15e980d767ac109372f9d32637f40600e20
yero95 <yero.beeusaert@leerling.molenland.be>
2026-05-06 11:03:59 +0200

    Speelveld methode validBlokjeCombo nog een fout

--- Speelveld.java
+++ Speelveld.java
@@ -7,35 +7,42 @@
  */
 public class Speelveld
 {
-    private Blokje[][] Speelveld = new Blokje[15][8];
+    private Blokje[][] speelveld = new Blokje[15][8];
     private Blokje blokje;
     
     boolean positieCheck(int rij, int kolom){
         boolean positieCorrect = false;
-        if(rij >= 0 && rij <= Speelveld.length){
-            if(kolom >= 0 && kolom <= Speelveld.length){
+        if(rij >= 0 && rij <= speelveld.length){
+            if(kolom >= 0 && kolom < speelveld.length){
                 positieCorrect = true;
             }
         }
         return positieCorrect;
     }
     
-    boolean validBlokjeCombo(Blokje b){
+    boolean validBlokjeCombo(Blokje nieuwBlokje, int rij , int kolom){
         boolean valid = false;
-        if(){
-            
+        Blokje vorigeBlokjeRij = speelveld[rij][kolom-1];
+        Blokje volgendBlokjeRij = speelveld[rij][kolom+1];
+        if(positieCheck(rij, kolom)){
+            if(vorigeBlokjeRij != null && volgendBlokjeRij != null){
+                if(vorigeBlokjeRij.getWaarde() == nieuwBlokje.getWaarde() - 1 || volgendBlokjeRij.getWaarde() == nieuwBlokje.getWaarde() + 1 ){
+                   valid = true;
+                }   
+            }
         }
+        return valid;
     }
     
     public void voegBlokjeToe(Blokje b, int rij, int kolom){
         if(positieCheck(rij, kolom)){
-            Speelveld[rij][kolom] = b;
+            speelveld[rij][kolom] = b;
         }
     }
     
     public void verwijderBlokje(int rij, int kolom){
         if(positieCheck(rij, kolom)){
-            Speelveld[rij][kolom] = null;
+            speelveld[rij][kolom] = null;
         }
     }
 }
