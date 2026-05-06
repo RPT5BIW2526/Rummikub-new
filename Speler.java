461ce6caca5e9595b84163cb305daed40144a67e
RPTolivierV <olivier.vandamme@leerling.molenland.be>
2026-05-06 10:29:07 +0200

    speler.klaar

--- Speler.java
+++ Speler.java
@@ -1,13 +1,76 @@
-public class Speler
-{
-    private String naam;
-    private Blokje blokjes;
-    private int punten;
-    private Bord bord;
+import java.util.ArrayList;
 
-    public Speler(String naam)
-    {
+public class Speler {
+
+    private String naam;
+    private int punten;
+    private ArrayList<Blokje> bord;
+    private boolean eersteBeurtGespeeld;
+
+    public Speler(String naam) {
         this.naam = naam;
-        punten = 0;
+        this.punten = 0;
+        this.bord = new ArrayList<>();
+        this.eersteBeurtGespeeld = false;
+    }
+
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getPunten() {
+        return punten;
+    }
+
+    public void setPunten(int punten) {
+        this.punten = punten;
+    }
+
+    public ArrayList<Blokje> getBord() {
+        return bord;
+    }
+
+    public int getAantalBlokjes() {
+        return bord.size();
+    }
+
+    public void voegBlokToeBord(Blokje blok) {
+        bord.add(blok);
+    }
+
+    public void verwijderBlokVanBord(Blokje blok) {
+        bord.remove(blok);
+    }
+
+    public int berekenPuntenBord() {
+        int totaal = 0;
+        for (Blokje blok : bord) {
+            totaal += blok.getWaarde();
+        }
+        return totaal;
+    }
+
+    public boolean heeftGeldigeEersteBeurt(ArrayList<Blokje> gekozenBlokjes) {
+        int totaal = 0;
+        for (Blokje blok : gekozenBlokjes) {
+            totaal += blok.getWaarde();
+        }
+        return totaal >= 30;
+    }
+
+    public boolean eersteBeurtGespeeld() {
+        return eersteBeurtGespeeld; 
+    }
+
+    public void setEersteBeurtGespeeld(boolean gespeeld) {
+        this.eersteBeurtGespeeld = gespeeld;
+    }
+
+    public boolean heeftGewonnen() {
+        return bord.isEmpty();
+    }
+
+    public String toString() {
+        return naam + " heeft " + bord.size() + " blokjes op zijn bord en " + punten + " punten.";
     }
 }
